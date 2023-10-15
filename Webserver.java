a9c7b4b9c9ebebab2d82a176e5191b3523525c1c
Patrick Sonnenberg <126255817+Nequeo2000@users.noreply.github.com>
2023-10-15 20:39:19 +0200

    Add files via upload

--- Webserver.java
+++ Webserver.java
@@ -13,10 +13,11 @@ import java.util.Scanner;
 import java.io.ByteArrayOutputStream;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+
 public class Webserver {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress("192.168.178.43",8000), 0);
-        server.createContext("/", new MyHandler());
+        HttpServer server = HttpServer.create(new InetSocketAddress("192.168.178.43",8080), 0);
+        server.createContext("/", new frontPage());
         server.createContext("/stream", new StreamHandler());
         
         server.setExecutor(null);
@@ -31,7 +32,18 @@ public class Webserver {
             ImageIO.write(img, "png", baos);
             byte[] bytes = baos.toByteArray();
 
-            t.sendResponseHeaders(200, bytes.length);
+            String userAgent = t.getRequestHeaders().get("user-agent").get(0).toLowerCase();
+            //System.out.println(userAgent);
+            t.getResponseHeaders().set("cache-control", "no-cache, must-revalidate, no-store");
+            t.getResponseHeaders().set("Accept-Encoding", "identity");
+            t.getResponseHeaders().set("Content-Type", "image/png");
+            if( userAgent.contains("firefox") ){
+                t.sendResponseHeaders(200, bytes.length*2);
+            } else {
+                t.sendResponseHeaders(200, bytes.length);
+            }
+
+            System.out.println(bytes.length);
             OutputStream os = t.getResponseBody();
             os.write(bytes);
             os.close();
@@ -43,7 +55,7 @@ public class Webserver {
             Scanner scanner = new Scanner(new FileReader(new File("./index.html")));
             String str = "";
             while(scanner.hasNextLine())
-                str += scanner.nextLine();
+                str += scanner.nextLine() +"\n";
             return str;
         }catch(Exception e){
             System.out.println(e);
@@ -51,7 +63,7 @@ public class Webserver {
         }
     }
 
-    static class MyHandler implements HttpHandler {
+    static class frontPage implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = Webserver.getFrontPage();
